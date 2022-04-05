6e9ca047371b8c4c010af845b34303ebd9e3bb85
Harsshanth <73587980+Harsshanth@users.noreply.github.com>
2022-04-05 22:37:37 +0530

    stack

---
+++ src/com/company/Stack.java
@@ -0,0 +1,71 @@
+package com.company;
+
+public class Stack {
+    Node top;
+    static int size = 0;
+
+    // NODE class
+    class Node {
+        int value;
+        Node next;
+        Node prev;
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    //push
+    public void push(int value) {
+        Node node = new Node(value);
+        if(top == null){
+            top = node;
+            return;
+        }
+        node.prev = top;
+        node.next = null;
+        top.next = node;
+        top = node;
+    }
+
+    public void pop() {
+        if(top == null)
+            return;
+        top = top.prev;
+        top.next = null;
+    }
+
+    public boolean search(int value) {
+        Node temp = top;
+        while(temp != null) {
+            if(temp.value == value)
+                return true;
+            temp = temp.prev;
+        }
+        return false;
+    }
+
+    public void display() {
+        Node temp = top;
+        while(temp != null) {
+
+            System.out.print(temp.value + "-->");
+            temp = temp.prev;
+        }
+        System.out.print("null");
+    }
+
+    public static void main(String[] args){
+        Stack stack = new Stack();
+        stack.push(5);
+        stack.push(8);
+        stack.push(3);
+        stack.push(6);
+        stack.display();
+        stack.pop();
+        System.out.println();
+        stack.display();
+        System.out.println("\n" + stack.search(6));
+
+    }
+
+}
