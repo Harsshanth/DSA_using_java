e3b21335ef1dc6b67f788fc07461db6caf8e89f0
Harsshanth <73587980+Harsshanth@users.noreply.github.com>
2022-04-26 09:40:55 +0530

    StringCrossArrangement

--- src/com/company/zoho/StringCrossArrangement.java
+++ src/com/company/zoho/StringCrossArrangement.java
@@ -7,15 +7,19 @@ public class StringCrossArrangement {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
         char ch[] = str.toCharArray();
-        ArrayList<Character> chR  = new ArrayList<Character>();
-        for(char c : ch) {
-            chR.add(c);
-        }
-        Collections.reverse(chR);
+        int m = ch.length-1;
         for(int i = 0; i < ch.length; i++) {
-            for(int j = ch.length - 1; j >= 0; j--) {
-
-            }
-        }
+           for(int j = 0; j < ch.length; j++) {
+               if(i == j && i + j != ch.length-1){
+                   System.out.print(ch[i]);
+               }else if(i + j == ch.length-1) {
+                   System.out.print(ch[m]);
+                   m--;
+               }else {
+                   System.out.print(" ");
+               }
+           }
+            System.out.println();
+       }
     }
 }
